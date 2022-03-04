834f0f7f4124bd6f064443494f8dd4bb36f6a504
nagaham <nagahama2015@gmail.com>
2022-03-04 14:14:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/progressbarsample/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.progressbarsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.progressbarsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.progressBar01.max = 100
+        binding.progressBar01.progress = 50
+        binding.progressBar01.secondaryProgress = 70
+
+        binding.button01.setOnClickListener { pushButton() }
+    }
+    fun pushButton(){
+        binding.progressBar02
+    }
+}
