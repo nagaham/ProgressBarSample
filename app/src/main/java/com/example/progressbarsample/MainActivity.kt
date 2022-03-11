46f30365e2686f4b3de068cad8d5b7fdd0186132
nagaham <nagahama2015@gmail.com>
2022-03-11 14:04:40 +0900

    add fragment

--- app/src/main/java/com/example/progressbarsample/MainActivity.kt
+++ app/src/main/java/com/example/progressbarsample/MainActivity.kt
@@ -6,6 +6,10 @@ import android.widget.ProgressBar
 import com.example.progressbarsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var barprogress = 0
+    var barsecond = 0
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +17,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.progressBar01.max = 100
-        binding.progressBar01.progress = 50
-        binding.progressBar01.secondaryProgress = 70
+        binding.progressBar01.progress = barprogress
+        binding.progressBar01.secondaryProgress = 0
 
-        binding.button01.setOnClickListener { pushButton() }
+        binding.buttonleft.setOnClickListener {
+            binding.progressBar01.progress = barprogress
+            pushButtonleft()
+        }
+        binding.button01.setOnClickListener { pushButtonright() }
     }
-    fun pushButton(){
-        binding.progressBar02.visibility = ProgressBar.INVISIBLE //
+    fun pushButtonright(){
+        binding.progressBar02.visibility = ProgressBar.INVISIBLE
+    }
+    fun pushButtonleft(){
+        barprogress++
     }
 }
